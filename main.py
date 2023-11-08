ca826d136316cae481134238609a55e4cb936291
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-11-07 22:34:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 
 import os
-print(os.listdir("c:/Users/Tuan Tran/Documents/GitHub/CECS450-project/datasets"))
+print(os.listdir("c:/Users/Tuan Tran/Documents/GitHub/CECS450-project/datasets")) # Change this path depend on where you saved your files
+
+# read in the data
+annual_co2_per_country = pd.read_csv("c:/Users/Tuan Tran/Documents/GitHub/CECS450-project/datasets/annual-co2-emissions-per-country.csv", header=0)
+annual_co2_per_country.head()
