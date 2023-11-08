dc9540ccd5360080a5077a5d832a4768076bc719
Tuan M Tran <tranhoangminhtuan86@gmail.com>
2023-11-07 20:47:41 -0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np # linear algebra
-# import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-# import matplotlib.pyplot as plt
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
 
 import os
 print(os.listdir("c:/Users/Tuan Tran/Documents/GitHub/CECS450-project/datasets"))
